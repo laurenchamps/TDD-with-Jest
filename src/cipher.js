b61b3ae2fefeb96b4baacd1dc88fa6f5459d27c5
Lauren Champs <laurenkchamps@gmail.com>
2024-01-05 17:05:17 +1030

    Refactor code in cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,30 @@
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+const getCase = function(char) {
+  if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
+    return 'upper';
+  }
+  if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
+    return 'lower';
+  }
+}
+
+const getNewIndex = function(char, shift) {
+  let curIndex;
+  let newIndex;
+
+  // Get current index of letter in alphabet array
+  curIndex = alphabet.indexOf(char.toLowerCase());
+  // Get new index
+  newIndex = curIndex + (shift % 26);
+    
+  // Wrap back to beginning of array when new index is above 25
+  if (newIndex > alphabet.length - 1) {
+      newIndex = newIndex - alphabet.length;
+    }
+  return newIndex;
+}
+
 const caesarCipher = function(string, shift) {
     if (typeof string !== 'string') return 'First argument must be a string';
     if (typeof shift !== 'number') return 'Second argument must be a number';
@@ -8,36 +33,17 @@ const caesarCipher = function(string, shift) {
     const encryptedArr = [];
   
     originalArr.forEach((char) => {
-      let curIndex;
-      let newIndex;
+      // If character is not a letter, add to encrypted array without conversion
+      if (getCase(char) !== 'upper' && getCase(char) !== 'lower') encryptedArr.push(char);
     
-      // Check if character is upper or lower case letter
-      if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
-        // Get current index of letter in alphabet array
-        curIndex = alphabet.indexOf(char.toLowerCase());
-        // Get new index
-        newIndex = curIndex + (shift % 26);
-        
-        // Wrap back to beginning of array when new index is above 25
-        if (newIndex > alphabet.length - 1) {
-          newIndex = newIndex - alphabet.length;
-        }
-        // Add new (encrypted) letter to encrypted array
-        encryptedArr.push(alphabet[newIndex].toUpperCase());
-      } else if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
-        // Get current index of letter in alphabet array
-        curIndex = alphabet.indexOf(char);
-        // Get new index
-        newIndex = curIndex + (shift % 26);
-        
-        // Wrap back to beginning of array when new index is above 25
-        if (newIndex > alphabet.length - 1) {
-          newIndex = newIndex - alphabet.length;
-        }
-        // Add new (encrypted) letter to encrypted array
-        encryptedArr.push(alphabet[newIndex]);
-      } else {
-        encryptedArr.push(char);
+      // If character is uppercase letter, get ciphered letter and add to array
+      if (getCase(char) === 'upper') {
+        encryptedArr.push(alphabet[getNewIndex(char, shift)].toUpperCase());
+      }
+      
+      // If character is lowercase letter, get ciphered letter and add to array
+      if (getCase(char) === 'lower') {
+        encryptedArr.push(alphabet[getNewIndex(char, shift)]);
       }
     });
   // Convert encrypted array into string
